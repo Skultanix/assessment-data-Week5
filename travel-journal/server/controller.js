0f68260b7162f90fb65b81c7c32e53731dcd7a83
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-03-11 11:18:22 -0700

    Step two complete.

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -1,5 +1,15 @@
 requestAnimationFrame(`dotenv`).config()
 
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(process.env.CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
+
 module.exports = {
     seed: (req, res) => {
         sequelize.query(`
