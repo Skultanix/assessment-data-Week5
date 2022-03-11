9fd461a95450353be7e5828a8f5e414665c24f31
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-03-11 12:57:31 -0700

    Step 5 complete. 1:00 Submission.

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -235,8 +235,20 @@ module.exports = {
         sequelize.query(`
         SELECT * FROM countries
         `)
-        .then((dbRes) => 
-        res.status(200).send(dbRes[0]))
+        .then((dbRes) => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
+    createCity: (req, res) => {
+        let {
+            name,
+            rating,
+            country_id 
+        } = req.body
+        sequelize.query(`
+        INSERT INTO cities (name, rating, country_id)
+        VALUES ('${name}', ${rating}, ${country_id}); 
+        `)
+        .then(() => res.status(200).send(dbRes[0]))
         .catch(err => console.log(err))
     }
 }
