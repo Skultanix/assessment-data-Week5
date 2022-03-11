da65d2d23a98fdb8c3cb5417e325900a4d92d69f
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-03-11 11:27:42 -0700

    Step 3 completed, donenv installed (oops).

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -21,7 +21,12 @@ module.exports = {
                 name varchar
             );
 
-            *****YOUR CODE HERE*****
+            CREATE TABLE cities (
+                city_id PRIMARY SERIAL KEY,
+                name VARCHAR,
+                rating INTEGER,
+                country_id INTEGER
+            );
 
             insert into countries (name)
             values ('Afghanistan'),
